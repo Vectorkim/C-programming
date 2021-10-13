bb82c3dafaeee90b7c0dd3cf16b60dea1cf3c6a8
Vectorkim <91733613+Vectorkim@users.noreply.github.com>
2021-10-13 11:26:30 +0300

    Update Perimeter and Area of a Rectangle.c

--- Perimeter and Area of a Rectangle.c
+++ Perimeter and Area of a Rectangle.c
@@ -5,9 +5,9 @@ int main()
 {
 int L, W;//L stands for Length and W stands for Width
 int Perimeter, Area;
-printf("Enter the length");//Prompt the user to enter a value for length
+printf("Enter the length\t");//Prompt the user to enter a value for length
 scanf("%d", &L);//Collect the input value for Length
-printf("\nEnter the width");//Prompt the user a value for width
+printf("\nEnter the width\t");//Prompt the user a value for width
 scanf("%d", &W);//Collect the input value for Width
 printf("\nPerimeter=2*Length+2*Width");//Display the formula for Perimeter
 Perimeter=2*L+2*W;//Calculate the Perimeter of the rectangle
