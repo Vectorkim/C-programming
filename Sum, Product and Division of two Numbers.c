173ab83c07e3181e7b5f9b67c96481eac0b2d5d3
Vectorkim <91733613+Vectorkim@users.noreply.github.com>
2021-10-14 13:19:23 +0300

    Update and rename Sum and Division of two Numbers to Sum, Product and Division of two Numbers.c

--- Sum, Product and Division of two Numbers.c
+++ Sum, Product and Division of two Numbers.c
@@ -1,17 +1,20 @@
-/*Arithmetic operations Sum and Division of two numbers*/
+/*Arithmetic operations Sum, Product and Division of two numbers*/
 #include <stdio.h>
 
 int main()
 {
-float a, b, Sum, Division;
+float a, b, Sum, Product, Division;
 printf("Enter Value a\t");//Prompt the user to Enter a value for a
 scanf("%f", &a);//Collect input values for a
 printf("\nEnter Value b\t");//Prompt the user to Enter a value for b
 scanf("%f", &b);//Collect input values for b
 printf("\nSum=a+b");//Display the formula for Sum
 Sum=a+b;//Calculate the sum
+printf("\nProduct=a*b");//Display the formula for Product
+Product=a*b;//Calculate the Product
 printf("\nDivision=a/b");//Display the formula for Division
 Division=a/b;//Divide the two numbers
 printf("\nSum=%f", Sum);//Display the result for Sum
+printf("\nProduct=%f", Product);//Display the Product
 printf("\nDivision=%f", Division);//Dispay the result for Division
 }
